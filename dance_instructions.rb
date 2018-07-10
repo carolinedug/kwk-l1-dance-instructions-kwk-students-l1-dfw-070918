9af0d42dd696a7c582d32fffd6d99fcdc8ad0ff0
caroline dugan <caroline.dugan@gmail.com>
2018-07-10 15:43:26 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -12,7 +12,7 @@ end
 def skip_step("left")
 puts "Lower #{left} foot to the ground"
 puts "Bounce #{left} foot back up slightly, kicking it a few inches back"
-puts "#{left}, Right, #{left} and #{left}"
+puts "Briefly Left, Right, Left, and Left"
 end
 
 def bob_the_reins
