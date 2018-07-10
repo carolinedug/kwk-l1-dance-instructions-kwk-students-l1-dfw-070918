1b795004b03463824ee94943e1b5e978706cead4
caroline dugan <caroline.dugan@gmail.com>
2018-07-10 15:35:20 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -31,3 +31,8 @@ puts "Start arm movement by holding left arm so the back of your wrist is near y
 puts "Lift right arm up and point right elbow diagonally to the right."
 puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
 end
+
+def bust_a_move
+puts "Be cool"
+puts "Be snazzy!"
+end
